2fb5bcbeec628f48fb0e610d869480c8f8e1d644
Telekrafter <artem.frolov2103@gmail.com>
2024-12-28 17:55:01 +0300

    ...

---
+++ Assets/scripts/moneta.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class moneta : MonoBehaviour
+{
+    private int monet;
+    public TextMeshProUGUI text_board;
+    // Start is called before the first frame update
+    void Start()
+    {
+        monet = 0;
+
+        text_board.text = "Черепов:" + monet.ToString();
+    }
+
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(monet);
+        monet ++;
+        text_board.text = "Черепов:" + monet.ToString();
+        Destroy(gameObject);
+
+
+    }
+}
--- Assets/scripts/sleket_move.cs
+++ Assets/scripts/sleket_move.cs
@@ -1,10 +1,12 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class sleket_move : MonoBehaviour
 {
-    private int skeleton_speed = 20;
+    private int monet;
+    public TextMeshProUGUI text_board;
+    private int skeleton_speed = 5;
     private Animator skelet_idet_bejit;
     Rigidbody rb;
     private bool ONground;
@@ -12,6 +14,9 @@ public class sleket_move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        monet = 0;
+
+        text_board.text = "Черепов:" + monet.ToString();
         skelet_idet_bejit = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
         ONground = true;
@@ -27,36 +32,36 @@ public class sleket_move : MonoBehaviour
             skelet_idet_bejit.Play("Walk");
 
         }
-        else
-        {  
-        skelet_idet_bejit.Play("Idle");
-        
-        }
-
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.back * Time.deltaTime * skeleton_speed);
             skelet_idet_bejit.Play("Walk");
 
         }
 
-        if (Input.GetKey(KeyCode.A))
+
+        else if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * Time.deltaTime * skeleton_speed);
             skelet_idet_bejit.Play("Walk");
 
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * Time.deltaTime * skeleton_speed);
             skelet_idet_bejit.Play("Walk");
 
         }
+        else
+        {
+            skelet_idet_bejit.Play("Idle");
+
+        }   
         if (Input.GetKey(KeyCode.Space) && ONground)
         {
             ONground = false;
             Debug.Log("space");
-            rb.AddForce(new Vector3(0, 500 ,0), ForceMode.Impulse);
+            rb.AddForce(new Vector3(0, 1000 ,0), ForceMode.Impulse);
             
 
         }
@@ -72,6 +77,17 @@ public class sleket_move : MonoBehaviour
             Debug.Log("spaceddd");
         }
 
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin"))
+        {
+            Debug.Log(monet);
+            monet++;
+            text_board.text = "Черепов:" + monet.ToString();
+            Destroy(other.gameObject);
+        }
 
     }
 
